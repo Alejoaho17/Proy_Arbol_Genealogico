7923918477ceddf1c5d0018fb1d15ed21ec57222
SZabala124 <167817001+SZabala124@users.noreply.github.com>
2024-11-11 19:17:50 -0400

    Update Tree.java

--- src/proy_2_arb_gen/Tree.java
+++ src/proy_2_arb_gen/Tree.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package gameofthrones;
+package proy_2_arb_gen;
 
 /**
  * Estructura de árbol general para representar una estructura o árbol
