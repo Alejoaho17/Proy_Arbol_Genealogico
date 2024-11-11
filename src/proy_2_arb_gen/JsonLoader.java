c807646719b9b3f8c215e1418707e8156cd2dc56
SZabala124 <167817001+SZabala124@users.noreply.github.com>
2024-11-11 19:17:06 -0400

    Update JsonLoader.java

--- src/proy_2_arb_gen/JsonLoader.java
+++ src/proy_2_arb_gen/JsonLoader.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package estructuras;
+package proy_2_arb_gen;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
